058537d568178f1a4d5afeedd13a8c3c4cf31636
Abreham <abrehamkassa19@gmail.com>
2025-01-21 17:46:49 +0300

    Add function that performs:     list tasks based on their status     mark a task as done or in-progress     update a task description

--- backend/task_tracker/src/main.rs
+++ backend/task_tracker/src/main.rs
@@ -1,5 +1,8 @@
-use std::{fs::OpenOptions, io::{Read, Write}};
-use clap::{Parser, Args, Subcommand, ValueEnum};
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use std::{
+    fs::OpenOptions,
+    io::{Read, Write},
+};
 // A struct that represent the task.
 #[derive(serde::Deserialize, serde::Serialize)]
 struct Task {
@@ -7,7 +10,17 @@ struct Task {
     description: String,
     status: String,
     created_at: chrono::DateTime<chrono::Utc>,
-    updated_at: chrono::DateTime<chrono::Utc>
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl std::fmt::Display for Task {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{{\n\tID: {}\n\tDescription: {}\n\tStatus: {}\n\tCreated_At: {}\n\tUpdated_At: {}\n}}",
+            self.id, self.description, self.status, self.created_at, self.updated_at
+        )
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -19,9 +32,8 @@ enum Mode {
     /// List tasks that we haven't yet begin doing
     Todo,
     /// List task that are in-progress
-    InProgress
+    InProgress,
 }
-
 
 #[derive(Args)]
 struct AddArgs {
@@ -32,7 +44,7 @@ struct AddArgs {
 #[derive(Args)]
 struct ListArgs {
     /// which type of tasks to list
-    mode: Mode
+    mode: Mode,
 }
 
 #[derive(Args)]
@@ -63,7 +75,7 @@ struct UpdateArgs {
     task_id: u16,
 
     /// Description of the task
-    description: String
+    description: String,
 }
 
 #[derive(Subcommand)]
@@ -88,13 +100,12 @@ enum Commands {
 }
 
 #[derive(Parser)]
-#[command(name="task_tracker")]
-#[command(version="1.0")]
+#[command(name = "task_tracker")]
+#[command(version = "1.0")]
 #[command(about = "Cli app to manage task", long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
-
 }
 
 fn main() {
@@ -104,28 +115,92 @@ fn main() {
         Commands::Add(add_args) => {
             add_task(&add_args.description);
         }
-        ,
         Commands::Delete(delete_args) => {
             delete_task(delete_args.task_id);
-        },
-        _ => {
-            
+        }
+        Commands::List(list_args) => {
+            let tasks = read_tasks();
+            if tasks.len() == 0 {
+                println!("Empty");
+            } else if list_args.mode == Mode::All {
+                for task in tasks {
+                    println!("{task}");
+                }
+            } else if list_args.mode == Mode::Done {
+                for task in tasks {
+                    if task.status == "done" {
+                        println!("{task}");
+                    } else {
+                        println!("Empty");
+                    }
+                }
+            } else if list_args.mode == Mode::InProgress {
+                for task in tasks {
+                    if task.status == "in-progress" {
+                        println!("{task}");
+                    } else {
+                        println!("Empty");
+                    }
+                }
+            } else if list_args.mode == Mode::Todo {
+                for task in tasks {
+                    if task.status == "todo" {
+                        println!("{task}");
+                    } else {
+                        println!("Empty");
+                    }
+                }
+            }
+        }
+        Commands::MarkDone(mark_done_args) => {
+            let mut tasks = read_tasks();
+
+            let index = tasks
+                .iter()
+                .position(|n| n.id == mark_done_args.task_id)
+                .expect("Task with the specified id does not exits");
+
+            tasks[index].status = "done".to_string();
+
+            write_tasks(tasks);
+            println!("marked successfully");
+        }
+        Commands::MarkInProgress(mark_in_progress_args) => {
+            let mut tasks = read_tasks();
+
+            let index = tasks
+                .iter()
+                .position(|n| n.id == mark_in_progress_args.task_id)
+                .expect("Task with the specified id does not extist");
+
+            tasks[index].status = "in-progress".to_string();
+            write_tasks(tasks);
+            println!("marked sucessfully");
+        }
+        Commands::Update(update_args) => {
+            let mut tasks = read_tasks();
+
+            let index = tasks
+                .iter()
+                .position(|n| n.id == update_args.task_id)
+                .expect("Task with the specified id does not extist");
+
+            tasks[index].description = update_args.description.clone();
+            write_tasks(tasks);
+            println!("updated sucessfully");
         }
     }
-    
-
-
 }
 
 fn add_task(description: &str) {
     let mut tasks = read_tasks();
 
-    let task = Task{
+    let task = Task {
         id: tasks.len() as u16 + 1,
         description: description.to_string(),
         status: "todo".to_string(),
         created_at: chrono::Utc::now(),
-        updated_at: chrono::Utc::now()
+        updated_at: chrono::Utc::now(),
     };
 
     tasks.push(task);
@@ -136,7 +211,10 @@ fn add_task(description: &str) {
 fn delete_task(task_id: u16) {
     let mut tasks = read_tasks();
 
-    let index = tasks.iter().position(|n| n.id == task_id).expect("The element with the specified id does not exist");
+    let index = tasks
+        .iter()
+        .position(|n| n.id == task_id)
+        .expect("The element with the specified id does not exist");
 
     let _ = tasks.remove(index);
 
@@ -145,10 +223,16 @@ fn delete_task(task_id: u16) {
 
 fn read_tasks() -> Vec<Task> {
     let mut content = String::new();
-    let mut file = OpenOptions::new().read(true).write(true).create(true).open("tasks.json").expect("Unable to open file.");
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("tasks.json")
+        .expect("Unable to open file.");
     let tasks: Vec<Task>;
 
-    file.read_to_string(&mut content).expect("Unable to read file");
+    file.read_to_string(&mut content)
+        .expect("Unable to read file");
 
     if content.trim().len() == 0 {
         tasks = Vec::new();
@@ -160,10 +244,14 @@ fn read_tasks() -> Vec<Task> {
 }
 
 fn write_tasks(tasks: Vec<Task>) {
-    let mut file = OpenOptions::new().write(true).truncate(true).create(true).open("tasks.json").expect("Unable to open file.");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("tasks.json")
+        .expect("Unable to open file.");
     let content = serde_json::to_string_pretty(&tasks).expect("Unable to convert into json");
 
-    file.write_all(&content.as_bytes()).expect("Unable to write to a file");
+    file.write_all(&content.as_bytes())
+        .expect("Unable to write to a file");
 }
-
-
