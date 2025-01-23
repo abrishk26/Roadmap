e1666b6979cd425731425cebd42e36e23ef2f902
Abreham <abrehamkassa19@gmail.com>
2025-01-23 19:21:41 +0300

    Add argument parsing to extract the username from the command line

--- backend/github_user_activity/index.js
+++ backend/github_user_activity/index.js
@@ -1,21 +1,28 @@
 const { Table } = require("console-table-printer");
 
-const response = await fetch("https://api.github.com/users/abrishk26/events", {
-    method: "GET",
-    headers: {"User-Agent": "abrishk26"}
-});
-
-const events = await response.json();
 const t = new Table({
     columns: [
         { name: "ID", alignment: "left", color: "blue" },
         { name: "Event Type", alignment: "left" },
-        { name: "Repo", alignment: "left"}
+        { name: "Repo", alignment: "left" }
     ]
 });
 
-for (let e of events) {
-    t.addRow({ ID: e["id"], "Event Type": e["type"], Repo: e["repo"]["name"]});
-}
+if (typeof Bun.argv[2] === "undefined") {
+    console.log("Please provide the user name and try again.");
+} else {
+    const username = Bun.argv[2];
 
-t.printTable();
+    const response = await fetch(`https://api.github.com/users/${username}/events`, {
+        method: "GET",
+        headers: { "User-Agent": "abrishk26" }
+    });
+
+    const events = await response.json();
+
+    for (let e of events) {
+        t.addRow({ ID: e["id"], "Event Type": e["type"], Repo: e["repo"]["name"] });
+    }
+
+    t.printTable();
+}
