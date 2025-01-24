6bd29c03e0acc8fdbeed8b516467330a119c6b06
Abreham <abrehamkassa19@gmail.com>
2025-01-24 15:32:00 +0300

    Add the summary, delete, list functionalities

--- backend/expense_tracker/main.py
+++ backend/expense_tracker/main.py
@@ -2,6 +2,27 @@ import typer
 import lib
 from datetime import date
 from typing_extensions import Annotated
+from prettytable import PrettyTable
+from typing import Optional
+
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+
+table = PrettyTable()
+table.field_names = ["ID", "Description", "Amoount", "Date"]
 
 app = typer.Typer(no_args_is_help=True)
 fileName = "./tasks.json"
@@ -24,19 +45,38 @@ def add(
     expenses.append(expense)
     lib.write_data(fileName, expenses)
 
+    print(f"Expense added successfully (ID: {expense["ID"]})")
+
 @app.command()
 def list():
     """
        list all the expenses.
     """
-    print("Listing expenses")
+    expenses = lib.read_data(fileName)
+
+    for expense in expenses:
+        table.add_row([expense["ID"], expense["Description"], expense["Amount"], expense["Date"]])
+
+    print(table)
 
 @app.command()
-def summary(month: int):
+def summary(month: Annotated[Optional[int], typer.Argument()] = None):
     """
         summarize the expense by calculating the total amount.
     """
-    print("Printing Summary")
+    totalExpense = 0
+    expenses = lib.read_data(fileName)
+    if month != None:
+        for expense in expenses:
+            if int(expense["Date"].split("-")[1]) == month:
+                totalExpense += expense["Amount"]
+        print(f"Total expenses for {months[month]}: {totalExpense}")
+        return
+       
+    for expense in expenses:
+        totalExpense += expense["Amount"]
+
+    print("Total expense:", totalExpense)
 
 @app.command()
 def delete(
@@ -47,7 +87,20 @@ def delete(
     """
         delete the expense with the given ID.
     """
-    print("User deleted successfully")
+    if force:
+        print("Deleting expense")
+    else:
+        print("Operation cancelled")
+    expenses = lib.read_data(fileName)
+
+    for i, expense in enumerate(expenses):
+        if expense["ID"] == id:
+            expenses.pop(i)
+            lib.write_data(fileName, expenses)
+            print("Deleting completed")
+            return
+
+    print("The expense with the specified ID does not exist")
 
 if __name__ == "__main__":
     app()
