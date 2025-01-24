51a88a3eb40409f3b5eac9b18865f766b4a4218b
Abreham <abrehamkassa19@gmail.com>
2025-01-24 22:44:09 +0300

    Add the generating random number and matching the user number functionality

--- backend/number-guessing-game/src/main.rs
+++ backend/number-guessing-game/src/main.rs
@@ -1,6 +1,83 @@
+use rand::Rng;
 use std::io::{self, Write};
 
 fn main() {
+    let mut play_again: bool = true;
+
+    while play_again {
+        let choice: u8 = start_game();
+
+        match choice {
+            1 => {
+                println!("Great! You have selected the Easy difficulty level.");
+                play_game(10);
+            }
+            2 => {
+                println!("Great! You have selected the Medium difficulty level.");
+                play_game(5);
+            }
+            3 => {
+                println!("Great! You have selected the Hard difficulty level.");
+                play_game(3);
+            }
+            4 => play_again = false,
+            _ => (),
+        };
+    }
+}
+
+fn play_game(chance: u8) {
+    let mut rng = rand::thread_rng();
+    let number: u8 = rng.gen_range(1..=100);
+    let mut attempt: u8 = 0;
+
+    println!("Let's start the game!\n");
+
+    while attempt < chance {
+        let mut input = String::new();
+
+        print!("Enter your guess: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let guess: u8 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                clearscreen::clear().expect("failed to clear screen");
+                println!("Invalid input. Please enter a number between 1 and 100.");
+                attempt += 1;
+                continue;
+            }
+        };
+
+        if guess < 1 || guess > 100 {
+            clearscreen::clear().expect("failed to clear screen");
+            println!("Invalid guess. Try again");
+            attempt += 1;
+            continue;
+        }
+
+        attempt += 1;
+
+        if guess < number {
+            println!("Incorrect! The number is greater than {guess}.");
+            println!();
+            continue;
+        } else if guess > number {
+            println!("Incorrect! The number is less than {guess}.");
+            println!();
+            continue;
+        }
+
+        println!("Congratulations! You guessed the correct number in {attempt} attempts.");
+        return;
+    }
+
+    println!("Failed! Try again.");
+    println!();
+}
+
+fn start_game() -> u8 {
     println!("Welcome to the Number Guessing Game!");
     println!("I'm thinking of a number between 1 and 100.");
     println!("You have 5 chances to guess the correct number.\n");
@@ -9,6 +86,7 @@ fn main() {
     println!("1. Easy (10 chances)");
     println!("2. Medium (5 chances)");
     println!("3. Hard (3 chances)");
+    println!("4. Exit");
 
     print!("Enter your choice: ");
     io::stdout().flush().unwrap();
@@ -19,7 +97,7 @@ fn main() {
 
     let mut choice: u8 = input.trim().parse().unwrap();
 
-    while choice < 1 || choice > 3 {
+    while choice < 1 || choice > 4 {
         clearscreen::clear().expect("failed to clear screen");
         println!("Invalid choice. Try again");
 
@@ -27,6 +105,7 @@ fn main() {
         println!("1. Easy (10 chances)");
         println!("2. Medium (5 chances)");
         println!("3. Hard (3 chances)");
+        println!("4. Exit");
 
         print!("Enter your choice: ");
         io::stdout().flush().unwrap();
@@ -38,35 +117,5 @@ fn main() {
         choice = input.trim().parse().unwrap();
     }
 
-    match choice {
-        1 => println!("Great! You have selected the Easy difficulty level."),
-        2 => println!("Great! You have selected the Medium difficulty level."),
-        3 => println!("Great! You have selected the Hard difficulty level."),
-        _ => (),
-    };
-
-    println!("Let's start the game!\n");
-
-    input.clear();
-    print!("Enter your guess: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let mut guess: u8 = input.trim().parse().unwrap();
-
-    while guess < 1 || guess > 100 {
-        clearscreen::clear().expect("failed to clear screen");
-        println!("Invalid guess. Try again");
-        
-        io::stdout().flush().unwrap();
-        
-        input.clear();
-        print!("Enter your guess: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut input).unwrap();
-
-        guess = input.trim().parse().unwrap();
-    }
-
-    println!("You have guessed {guess}");
+    choice
 }
