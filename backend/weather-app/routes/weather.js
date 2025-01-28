3875fd01860d7067b252dcbfa7f604b3c79a4878
Abreham <abrehamkassa19@gmail.com>
2025-01-28 17:42:51 +0300

    Update the weather endpoint handler to construct the request query and make the request and send a response on success and log error when it occurs.

--- backend/weather-app/routes/weather.js
+++ backend/weather-app/routes/weather.js
@@ -3,10 +3,29 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     require('dotenv').config();
-    console.log(process.env.API_KEY);
+    const key = process.env.API_KEY;
     const city = req.query.city;
-    console.log(city);
-    res.render('weather')
+
+    const date = new Date();
+    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    
+    const baseUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${formattedDate}`;
+    const queryParam = {
+        key: key
+    };
+
+    const queryString = new URLSearchParams(queryParam).toString();
+    const requestUrl = `${baseUrl}?${queryString}`;
+
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(data => {
+            // console.log('Response:', data);
+            res.render('weather', { data })
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
 });
 
 module.exports = router;
