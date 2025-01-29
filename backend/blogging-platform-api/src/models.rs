ada84595f1c66083121230d16cba6b3ebf5d34b6
Abreham <abrehamkassa19@gmail.com>
2025-01-29 12:14:10 +0300

    Make the fields of the Post struct visible for an outside module

--- backend/blogging-platform-api/src/models.rs
+++ backend/blogging-platform-api/src/models.rs
@@ -1,11 +1,11 @@
 pub use chrono::prelude::*;
 
 pub struct Post {
-    id: Option<i32>,
-    title: String,
-    content: String,
-    category: String,
-    tags: Vec<String>,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>
+    pub id: i32,
+    pub title: String,
+    pub content: String,
+    pub category: String,
+    pub tags: Vec<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>
 }
