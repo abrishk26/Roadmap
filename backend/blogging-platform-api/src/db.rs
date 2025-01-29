23ce8b34647bb6f4bf64a15309c35041cf4f2c2e
Abreham <abrehamkassa19@gmail.com>
2025-01-29 11:03:09 +0300

    Add function to create a pool of database connection

---
+++ backend/blogging-platform-api/src/db.rs
@@ -0,0 +1,14 @@
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
+use std::error::Error;
+use dotenvy::dotenv;
+
+pub async fn establish_connection() -> Result<PgPool, Box<dyn Error>> {
+    dotenv()?;
+    let database_url = env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url).await?;
+    return pool;
+}
