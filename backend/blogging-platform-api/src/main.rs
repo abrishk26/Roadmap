2c5ed4337ed8d27ce24349d16ee13bdb800696ad
Abreham <abrehamkassa19@gmail.com>
2025-01-30 00:35:14 +0300

    Defined the server and registered handlers into it

--- backend/blogging-platform-api/src/main.rs
+++ backend/blogging-platform-api/src/main.rs
@@ -1,5 +1,30 @@
 mod handlers;
+use actix_web::{middleware::Logger, App, HttpServer, web};
+use env_logger::Env;
+use sqlx::PgPool;
+use db_helper::establish_connection;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone)]
+pub struct AppState {
+    pub conn: PgPool,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let app_state = web::Data::new(AppState {
+        conn: establish_connection().await.unwrap()
+    });
+    
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .app_data(app_state.clone())
+            .service(handlers::create)
+            .service(handlers::get_post)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
