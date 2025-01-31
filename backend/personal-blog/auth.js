d2bd41b0728c0b254abfdcea9c73113ea7975954
Abreham <abrehamkassa19@gmail.com>
2025-01-31 12:20:48 +0300

    Add config object that used to store the filename and other configurations if needed

---
+++ backend/personal-blog/auth.js
@@ -0,0 +1,9 @@
+const isAuthenticated = (req, res, next) => {
+  if (req.session.isAuthenticated) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+module.exports = isAuthenticated;
--- backend/personal-blog/config.js
+++ backend/personal-blog/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  articlesFilePath: path.join(__dirname, "articles.jons")
+  articlesFilePath: path.join(__dirname, "articles.json")
 };
 
 module.exports = Object.freeze(config);
