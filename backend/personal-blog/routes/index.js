be8f831c33b0757e827aa016b713ca3d68ca3581
Abreham <abrehamkassa19@gmail.com>
2025-01-31 07:59:44 +0300

    Add the landing page

--- backend/personal-blog/routes/index.js
+++ backend/personal-blog/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const router = express.Router();
 
+const articles = [
+  { title: "First Title", date: "22-32-32" },
+  { title: "Second Title", date: "23-32-32" }
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { articles: articles });
 });
 
 module.exports = router;
