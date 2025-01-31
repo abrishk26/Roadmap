a42862ac3cdba30da51257b83705836565586764
Abreham <abrehamkassa19@gmail.com>
2025-01-31 12:27:30 +0300

    Add functions that handle request related with article

---
+++ backend/personal-blog/routes/article.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const isAuthenticated = require('../auth');
+const file = require('../file-handler');
+
+
+router.get("/article/:id", (req, res) => {
+  const articles = file.readArticles();
+  const articleId = parseInt(req.params.id);
+  const article = articles.find((a) => a.id === articleId);
+
+  if (article) {
+    res.render("article", { article });
+  } else {
+    res.status(404).send("Article not found");
+  }
+});
+
+router.get('/new', isAuthenticated, (req, res) => {
+  res.render('new-article');
+});
+
+router.get('/update/:id', isAuthenticated, (req, res) => {
+  res.render('update-article', { article: { id: req.params.id } });
+});
+
+router.get('/delete/:id', isAuthenticated, (req, res) => {
+  const articles = file.readArticles();
+  const articleId = parseInt(req.params.id);
+  
+  const article = articles.find(a => a.id === articleId)
+  const index = articles.indexOf(article);
+  
+  if (index > -1) {
+    articles.pop(index);
+    file.writeArticles(articles);
+  }
+  
+  res.redirect('/admin')
+})
+
+router.post('/new', isAuthenticated, (req, res) => {
+  const articles = file.readArticles();
+  const id = 1
+  if (articles.length > 0) {
+    id = articles[articles[(articles.length - 1).toString()]] + 1
+  }
+  const newArticle = {
+    id,
+    title: req.body.title,
+    date: req.body.date,
+    content: req.body.content
+  };
+  
+  articles.push(newArticle);
+  file.writeArticles(articles);
+  res.redirect("/admin")
+});
+
+router.post('/update/:id', isAuthenticated, (req, res) => {
+  const articles = file.readArticles();
+  const articleId = parseInt(req.params.id);
+  console.log(articleId);
+  const article = articles.find(a => a.id === articleId);
+  
+  if (article) {
+    article.title = req.body.title;
+    article.date = req.body.date;
+    article.content = req.body.content;
+  
+    file.writeArticles(articles);
+  } else {
+    console.log('Article not found.');
+  }
+  
+  res.redirect('/admin')
+})
+
+module.exports = router;
