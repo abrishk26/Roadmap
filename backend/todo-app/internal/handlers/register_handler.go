152d425559516267db9f98bce11238378cb3ff05
Abreham <abrehamkassa19@gmail.com>
2025-02-04 11:52:29 +0300

    Update the signature

--- backend/todo-app/internal/handlers/register_handler.go
+++ backend/todo-app/internal/handlers/register_handler.go
@@ -2,63 +2,66 @@ package handlers
 
 import (
 	"context"
-	"github.com/gin-gonic/gin"
-	"github.com/abrishk26/Roadmap/backend/todo-app/internal/models"
-	"github.com/abrishk26/Roadmap/backend/todo-app/internal/repositories"
-	"golang.org/x/crypto/bcrypt"
-	"github.com/google/uuid"
 	"net/http"
+
+	"github.com/abrishk26/Roadmap/backend/todo-app/internal/models"
+	repository "github.com/abrishk26/Roadmap/backend/todo-app/internal/repositories"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
-	Name string `json:"name" binding:"required"`
-	Email string `json:"email" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
-func RegisterHandler(c *gin.Context, userRepo *repository.UserRepository) {
-	var req RegisterRequest
-	
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid request",
+func RegisterHandler(c *gin.Context, userRepo *repository.UserRepository) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req RegisterRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to hash passwrod",
+			})
+			return
+		}
+
+		ctx := context.Background()
+		newUser := models.User{
+			ID:           uuid.New(),
+			Name:         req.Name,
+			Email:        req.Email,
+			PasswordHash: string(hashedPassword),
+		}
+
+		user, err := userRepo.Create(ctx, &newUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to register user",
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "User successfully registered",
+			"user": gin.H{
+				"id":    user.ID,
+				"name":  user.Name,
+				"email": user.Email,
+			},
 		})
-		return
 	}
-	
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-	
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error" : "Failed to hash passwrod",
-		})
-		return
-	}
-	
-	ctx := context.Background()
-	newUser := models.User {
-		ID: uuid.New(),
-		Name: req.Name,
-		Email: req.Email,
-		PasswordHash: string(hashedPassword),
-	}
-	
-	user, err := userRepo.Create(ctx, &newUser)
-	
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to register user",
-		})
-		
-		return
-	}
-	
-	c.JSON(http.StatusOK, gin.H{
-		"message": "User successfully registered",
-		"user" : gin.H{
-			"id" : user.ID,
-			"name": user.Name,
-			"email": user.Email,	
-		},
-	})
 }
