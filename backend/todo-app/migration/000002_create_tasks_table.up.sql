93d43ddbf3ed536317dab15b475fb079b0982ef6
Abreham <abrehamkassa19@gmail.com>
2025-02-04 08:14:06 +0300

    Update the tasks table definition to include foreign key column

--- backend/todo-app/migration/000002_create_tasks_table.up.sql
+++ backend/todo-app/migration/000002_create_tasks_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS tasks (
     task_id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL, -- Added user_id column
     title TEXT UNIQUE NOT NULL,
     description TEXT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
