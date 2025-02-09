4583e0ce0cf46715e133dc42af4dcc50e4616615
Abreham <abrehamkassa19@gmail.com>
2025-02-10 00:48:25 +0300

    Add functionalities to the main app     - load variables from .env file to the environment varialbe     - extract api-key from the environment variable     - parse the command line argument and make request base on the argument     - make a request for the given argument

--- backend/tmdb-cli/src/main.rs
+++ backend/tmdb-cli/src/main.rs
@@ -1,6 +1,71 @@
 mod cli;
+mod models;
 use clap::Parser;
+use cli::Mode;
+use dotenvy::dotenv;
+use reqwest::blocking::Client;
+use std::env;
 
 fn main() {
-    let _ = cli::Cli::parse();
+    // Load variables
+    match dotenv() {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("failed to load variables {err}");
+            return;
+        }
+    }
+
+    // Set up the argument parser
+    let app = cli::Cli::parse();
+
+    // Retrieve the api-key from the environment
+    let api_key: String;
+    match env::var("API_KEY") {
+        Ok(key) => api_key = key,
+        Err(err) => {
+            eprintln!("env variable API_KEY must be set: {err}");
+            return;
+        }
+    }
+
+    match app.r#type {
+        Mode::PLAYING => {
+            let response = make_request("https://api.themoviedb.org/3/movie/now_playing", &api_key);
+            match response {
+                Ok(res) => println!("{:?}", res),
+                Err(err) => println!("Error: {err}"),
+            }
+        }
+        Mode::POPULAR => {
+            let response = make_request("https://api.themoviedb.org/3/movie/popular", &api_key);
+            match response {
+                Ok(res) => println!("{:?}", res),
+                Err(err) => println!("Error: {err}"),
+            }
+        }
+        Mode::TOP => {
+            let response = make_request("https://api.themoviedb.org/3/movie/top_rated", &api_key);
+            match response {
+                Ok(res) => println!("{:?}", res),
+                Err(err) => println!("Error: {err}"),
+            }
+        }
+        Mode::UPCOMING => {
+            let response = make_request("https://api.themoviedb.org/3/movie/upcoming", &api_key);
+            match response {
+                Ok(res) => println!("{:?}", res),
+                Err(err) => println!("Error: {err}"),
+            }
+        }
+    }
+
+}
+
+fn make_request(url: &str, api_key: &str) -> Result<models::Res, Box<dyn std::error::Error>> {
+    let client = Client::new()
+        .get(url)
+        .header("Authorization", "Bearer ".to_owned() + api_key);
+    let response: models::Res = client.send()?.json()?;
+    Ok(response)
 }
